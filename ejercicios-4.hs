f935a4988fe671276ac681b9237a9762508f9eab
lulojrz <109257773+lulojrz@users.noreply.github.com>
2025-04-28 12:21:00 -0300

    Update ejercicios-4.hs

--- ejercicios-4.hs
+++ ejercicios-4.hs
@@ -185,5 +185,21 @@ nEsimoPrimoAux n cont aux | esPrimo aux && cont == n-1 = aux
 
 
 
+--ejercicio 17
+esFibonacci :: Integer -> Bool
+esFibonacci n = aux n 0
+
+aux :: Integer -> Integer -> Bool
+aux n cont | n == fibonacci cont = True
+           | fibonacci cont > n = False
+           | otherwise = aux n (cont+1)
+        
+
+fibonacci :: Integer -> Integer
+fibonacci n | n==0 = 0
+            | n==1 = 1
+            | otherwise = fibonacci(n-1) + fibonacci(n-2)
+
+
 
 
